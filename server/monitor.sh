eb7ef708f8384d06a5713faa0f60d93339f18578
Maximiliano Churichi <mchurichi@gmail.com>
2015-05-16 00:53:09 -0300

    Improved the bash script to switch the interface to monitor mode

--- server/monitor.sh
+++ server/monitor.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
-ifconfig wlp4s0 down
-iwconfig wlp4s0 mode Monitor
-ifconfig wlp4s0 up
 
+DEFAULT_IF='wlan0'
+
+if [ -n "$1" ]; then
+  IF=$1
+  echo Using interface $1
+else
+  IF=DEFAULT_IF
+  echo Interface not specified, using $DEFAULT_IF
+fi
+
+if [ `whoami` != 'root' ]; then
+  echo Error: Current user is `whoami`, please run as root. Exiting.
+  exit 1
+fi
+
+ifconfig -s $IF &> /dev/null
+if [ $? -ne 0 ]; then
+  echo Interface $IF does not exists. Exiting.
+  exit 1
+fi
+
+ifconfig $IF down
+iwconfig $IF mode Monitor
+ifconfig $IF up
+
+if [ $? -eq 0 ]; then
+  echo $IF was restarted in Monitor mode
+fi
